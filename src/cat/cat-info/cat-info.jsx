99e6aecbe0cea071d94f5f3cadf8b8a5f1aca084
Lady Sun <ladysun.freedom@gmail.com>
2019-08-26 21:59:58 +0200

    cat-info: Created base card

--- src/cat/cat-info/cat-info.jsx
+++ src/cat/cat-info/cat-info.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import Card from 'react-bootstrap/Card'
 
 const CatInfo = () => {
-  return ( <div>CatInfo</div> );
+  return ( 
+    <Card style={{marginTop: '20px'}}>
+      <Card.Body>
+        <Card.Title>🐱 1 - Namecat</Card.Title>
+        <Card.Subtitle className="mb-2 text-muted">♂️ 18/08/2002</Card.Subtitle>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the bulk of
+          the card's content.
+        </Card.Text>
+      </Card.Body>
+    </Card> 
+  );
 }
  
 export default CatInfo;
