61a9ffc77d16a8b56f5fa8ad010632e9dd655cd7
Lady Sun <ladysun.freedom@gmail.com>
2019-09-22 18:25:00 +0200

    [contact] Creted form utils

--- src/contact/contact.jsx
+++ src/contact/contact.jsx
@@ -2,55 +2,29 @@ import React, { useState } from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import * as yup from 'yup';
-import { validationCondition } from 'jest-validate/build/condition';
+import {
+  createSchemaForm,
+  createSchemaValidationForm,
+  setValueForm,
+  handleSubmitForm
+} from '../core/utils/form';
 
 const Contact = () => {
-  // utils create form structure
-  const [contactInfo, setContactInfo] = useState({
-    user: { value: '', msgError: '' },
-    mail: { value: '', msgError: '' },
-    content: { value: '', msgError: '' },
-  });
-  // utils create schema
-  const validationSchema = yup.object().shape({
-    user: yup.object().shape({ msgError: yup.string(), value: yup.string().required('The username is required')}),
-    mail: yup.object().shape({ msgError: yup.string(), value: yup.string().email('This must be an email').required('The mail is required') }),
-    content: yup.object().shape({ msgError: yup.string(), value: yup.string().required('The content is required')}), 
-  });
-  // utils setvalue
-  const setValueForm = (value, name) => { 
-    const contactInfoAux = { 
-      ...contactInfo, 
-      [name]: {...contactInfo[name], value} 
-    };
-    setContactInfo(contactInfoAux);
+  const schemaForm = {
+    user: yup.string().required('The username is required'),
+    mail: yup.string().email('This must be an email').required('The mail is required'),
+    content: yup.string().required('The content is required')
   };
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    event.stopPropagation();
-    // utils rewind
-    for(const key in contactInfo) {
-      contactInfo[key].msgError = '';
-    }
-    // utils validation
-    validationSchema.validate(contactInfo, {abortEarly: false}).catch(valid => {
-      const contactInfoAux = {...contactInfo};
-      valid.inner.forEach( rawVal => {
-        const key = rawVal.path.split('.')[0];
-        contactInfoAux[key].msgError = rawVal.message;
-      });
-      setContactInfo(contactInfoAux);
-    });
-  };
-
+  const [contactInfo, setContactInfo] = useState(createSchemaForm(schemaForm));
+  const validationSchema = createSchemaValidationForm(schemaForm);
   return ( 
-    <Form noValidate onSubmit={handleSubmit}>
+    <Form noValidate onSubmit={(event) => handleSubmitForm(event, validationSchema, contactInfo, setContactInfo)}>
       <Form.Group>
         <Form.Control 
           type="text" 
           placeholder="User" 
           value={contactInfo.user.value} 
-          onChange={(event) => setValueForm(event.target.value, 'user')}
+          onChange={(event) => setValueForm(event.target.value, 'user', contactInfo, setContactInfo)}
         />
         <Form.Text className="text-muted">
           { contactInfo.user.msgError }
@@ -61,7 +35,7 @@ const Contact = () => {
           type="mail" 
           placeholder="Mail contact"
           value={contactInfo.mail.value} 
-          onChange={(event) => setValueForm(event.target.value, 'mail')}
+          onChange={(event) => setValueForm(event.target.value, 'mail', contactInfo, setContactInfo)}
         />
         <Form.Text className="text-muted">
           { contactInfo.mail.msgError }
@@ -72,7 +46,7 @@ const Contact = () => {
           as="textarea" 
           placeholder="What do you want?"
           value={contactInfo.content.value} 
-          onChange={(event) => setValueForm(event.target.value, 'content')}
+          onChange={(event) => setValueForm(event.target.value, 'content', contactInfo, setContactInfo)}
         />
         <Form.Text className="text-muted">
           { contactInfo.content.msgError }
---
+++ src/core/utils/form.js
@@ -0,0 +1,56 @@
+// Utils for first level form
+// Todo nested form
+import * as yup from 'yup';
+
+const createSchemaForm = (schema) => {
+    const schemaForm = {};
+    for (const key in schema) {
+        schemaForm[key] = { value: '', msgError: '' }
+    };
+    return schemaForm;
+};
+
+const createSchemaValidationForm = (schemaForm) => {
+    const schemaValidationForm = {};
+    for (const key in schemaForm) {
+        schemaValidationForm[key] = yup.object().shape({
+            msgError: yup.string(),
+            value: schemaForm[key]
+        });
+    };
+    return yup.object().shape(schemaValidationForm);
+}
+
+const setValueForm = (value, name, stateForm, setStateForm) => {
+    const contactInfoAux = {
+        ...stateForm,
+        [name]: { ...stateForm[name], value }
+    };
+    setStateForm(contactInfoAux);
+};
+
+// Todo Refactor handleSubmitForm
+const handleSubmitForm = (event, validationSchema, stateForm, setStateForm) => {
+    event.preventDefault();
+    event.stopPropagation();
+    // utils rewind
+    for (const key in stateForm) {
+        stateForm[key].msgError = '';
+    }
+    // utils validation
+    validationSchema.validate(stateForm, { abortEarly: false }).catch(valid => {
+        const stateFormAux = { ...stateForm };
+        valid.inner.forEach(rawVal => {
+            const key = rawVal.path.split('.')[0];
+            stateFormAux[key].msgError = rawVal.message;
+        });
+        setStateForm(stateFormAux);
+    });
+};
+
+export {
+    createSchemaForm,
+    createSchemaValidationForm,
+    setValueForm,
+    handleSubmitForm
+};
