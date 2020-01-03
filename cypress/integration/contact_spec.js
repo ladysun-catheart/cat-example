a3d878073a512f8f39eb8f2c91a9b3ffb83c79e1
Lady Sun <ladysun.freedom@gmail.com>
2020-01-03 17:07:50 +0100

    [e2e-test] Minimal cypress structure

---
+++ cypress/integration/contact_spec.js
@@ -0,0 +1,19 @@
+context('Given a contact page...', () => {
+
+    it('You can send a message', () => {
+
+    })
+
+    it('You can send several messages', () => {
+        
+    })
+
+    it('You cannot send a message with wrong mail', () => {
+        
+    })
+
+    it('you cannot send a message unless you type the mandatory fields', () => {
+        
+    })
+
+})
---
+++ cypress/integration/main_spec.js
@@ -0,0 +1,19 @@
+context('Given a main page...', () => {
+
+    it('There is a table and no cat description', () => {
+        cy.visit('http://localhost:3000')
+    })
+
+    it('You can select a cat to see its description', () => {
+
+    })
+
+    it('You can search cats', () => {
+
+    })
+
+    it('You can download cat info', () => {
+
+    })
+
+})
