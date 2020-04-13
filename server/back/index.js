e9e508f514fb0271d5b10e2bec9e374b5d254908
Lady Sun <ladysun.freedom@gmail.com>
2020-04-13 02:08:17 +0200

    [cat-server] refactor index.js server

--- server/back/index.js
+++ server/back/index.js
@@ -6,29 +6,42 @@ const catController = require('./controller/CatController')
 const PORT = 9001
 const PATH_DB = 'mongodb://localhost:9010/neko'
 
-
-const getDbConnection = async function () {
-	const optsDb = {
-		user: 'nekoUser',
-		pass: 'nekoUser',
-		useNewUrlParser: true,
-		useUnifiedTopology: true
-	}
-	const conn = mongoose.createConnection(PATH_DB, optsDb)
-		.then(() => console.log('DBS conected'))
-		.catch(() => console.log('DBS fail'))
-	return conn
-}
-
-const init = function () {
-	const app = express()
-	app.use(express.json())
-	app.use(morgan(':method :url :status - :response-time ms'))
-	app.use('/cat', catController)
-	getDbConnection()
-	app.listen(PORT, () => {
-		console.log(`Server listen in port ${PORT}`);
+const dbConnection = function () {
+	return new Promise((res, rej) => {
+		const optsDb = {
+			user: 'nekoUser',
+			pass: 'nekoUser',
+			useNewUrlParser: true,
+			useUnifiedTopology: true
+		}
+		const conn = mongoose.createConnection(PATH_DB, optsDb)
+			.then(() => {
+				console.log('DBS conected')
+				res(conn)
+			})
+			.catch(() => rej('DBS fail connection'))
 	})
 }
 
-init()
+const initApi = function (conn) {
+	return new Promise((res, rej) => {
+		const app = express()
+		app.use(express.json())
+		app.use(morgan(':method :url :status - :response-time ms'))
+		app.use('/cat', catController)
+		app.listen(PORT, () => {
+			console.log(`Api listen in port ${PORT}`);
+			res()
+		})
+	})
+}
+
+const initServer = function () {
+	console.log('Initialize environment...')
+	dbConnection()
+		.then(initApi)
+		.then(() => console.log('Server was initialized successfully!'))
+		.catch(err => console.log(err))
+}
+
+initServer()
